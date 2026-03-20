bde687ab2e223a9ea5da69e37e15a02193ca9879
tymongwp <tymonaa06@gmail.com>
2026-03-20 12:11:47 +0000

    Update scannerr.py

--- scannerr.py
+++ scannerr.py
@@ -4,13 +4,13 @@ target = "127.0.0.1"
 
 ports = [443,80,8080,21,22]
 
-for ports in ports:
+for port in ports:
     sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     sock.settimeout(1)
-    result=sock.connect_ex((target,ports))
+    result=sock.connect_ex((target,port))
     if result==0:
-        print(f"Port {ports} is Open")
+        print(f"Port {port} is Open")
     else:
-        print(f"Port {ports} is Closed")
+        print(f"Port {port} is Closed")
     
     sock.close()
