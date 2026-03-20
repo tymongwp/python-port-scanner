b461d7752e1023d8e87a07e6b0ab9468fa55b6b1
tymongwp <tymonaa06@gmail.com>
2026-03-20 11:53:22 +0000

    Update scannerr.py

--- scannerr.py
+++ scannerr.py
@@ -1,45 +1,16 @@
 import socket
-import threading
 
-print_lock = threading.Lock()
+def port_scanner(ip, ports):
 
+    print(f"Scanning {ip}...")
+    for port in ports:
+      with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+          s.settimeout(.5)
+          if s.connect_ex((ip, port)) == 0:
+            print(f"Port {port} is open.")
 
-def scan_port(target, port):
-    try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.5)
-
-        result = sock.connect_ex((target, port))
-
-        if result == 0:
-            with print_lock:
-                print(f"[OPEN] Port {port}")
-
-        sock.close()
-
-    except:
-        pass
-
-
-def scan_ports(target, start_port, end_port):
-    print(f"\n[+] Scanning {target} from port {start_port} to {end_port}\n")
-
-    threads = []
-
-    for port in range(start_port, end_port + 1):
-        t = threading.Thread(target=scan_port, args=(target, port))
-        threads.append(t)
-        t.start()
-
-    for t in threads:
-        t.join()
-
-    print("\n[+] Scan complete.")
-
-
-if __name__ == "__main__":
-    target = input("Enter target IP or domain: ")
-    start = int(input("Start port: "))
-    end = int(input("End port: "))
-
-    scan_ports(target, start, end)
+# Example 
+if _name_ == "__main__":
+    target_ip = "192.168.0.1"  
+    target_ports = range(20, 300)  
+    port_scanner(target_ip, target_ports)
